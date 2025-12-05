fa97d4cad9d48ac403662ee44093b99f4526f880
AgungTrivaldo <yantosputra121@gmail.com>
2025-12-05 14:45:14 +0700

    adding third task for the first dag

--- dags/first_dag.py
+++ dags/first_dag.py
@@ -1,27 +1,36 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 
-default_args = {
-    'owner': 'Agung',
-    'retries': '5',
-    'retry_delay': timedelta(minutes=2)
-}
+default_args = {"owner": "Agung", "retries": "5", "retry_delay": timedelta(minutes=2)}
 
 with DAG(
-    dag_id = 'First_Dag_v2', # DAG Name
-    default_args = default_args,
-    description = 'This is my First Dag', # DAG description
-    start_date = datetime(2025,1,1),# DAG Start Date
-    schedule_interval = '@daily', # DAG frequency 
+    dag_id="First_Dag_v3",  # DAG Name
+    default_args=default_args,
+    description="This is my First Dag",  # DAG description
+    start_date=datetime(2025, 1, 1),  # DAG Start Date
+    schedule_interval="@daily",  # DAG frequency
 ) as dag:
     task1 = BashOperator(
-        task_id = 'first_task', # task id or task name
-        bash_command = 'echo This is my first DAG' # command
+        task_id="first_task",  # task id or task name
+        bash_command="echo This is my first DAG",  # command
     )
 
     task2 = BashOperator(
-        task_id = 'second_task',
-        bash_command = 'echo This is the second task'
+        task_id="second_task", bash_command="echo This is the second task"
     )
-    task1.set_upstream(task2) # bakal nyambung ke task yang pertama jadi Task1 -> Task2
+    task3 = BashOperator(
+        task_id="second_task",
+        bash_command="echo This is the third task that run parallel with the second task",
+    )
+    task1.set_downstream(
+        task2
+    )  # bakal nyambung ke task yang pertama jadi Task1 -> Task2
+
+    # task1.set_upstream(
+    #     task2
+    # )  # bakal nyambu ke task yang pertama tapi jadinya Task2 -> Task1
+
+    task1.set_downsteam(
+        task3
+    )  # kalo semisam ada 2 downstream maka task yang nyambung ke parentnya jadi 2 task dan berjalan bersamaan
