ba4d415c1677f303407d9d6bb9e4a251eb493a44
AgungTrivaldo <yantosputra121@gmail.com>
2025-12-05 13:06:26 +0700

    adding test dag

---
+++ dags/stock_market.py
@@ -0,0 +1,11 @@
+from airflow.decorators import dag
+from datetime import datetime
+
+@dag(
+    start = datetime(2025,1,1),
+    schedule = '@daily',
+    catchup = False,
+    tags = ['stock']
+    )
+def stock_marker():
+    pass
