554bd73030bcc585b186d119c09272782031e309
AgungTrivaldo <yantosputra121@gmail.com>
2025-12-06 21:52:22 +0700

    adding taskflow

--- dags/taskflow.py
+++ dags/taskflow.py
@@ -2,31 +2,34 @@ from datetime import timedelta, datetime
 from airflow.decorators import dag, task
 
 
-default_args = {"owner": "agung", "retries": "5", "retry_delay": timedelta(minutes=5)}
+default_args = {"owner": "agung", "retries": 5, "retry_delay": timedelta(minutes=5)}
 
 
 @dag(
-    dag_id="first_taskflow",
+    dag_id="first_taskflowv1",
     default_args=default_args,
     schedule_interval="@once",
     start_date=datetime(2025, 1, 1),
 )
 def hello():
-    @task()
+    @task(multiple_outputs=True)
     def get_name():
-        return "yanto"
+        return {
+            'firstname':'Yanto',
+            'lastname':'Suyanto'
+        }
 
     @task()
     def get_age():
         return 5
 
     @task()
-    def greet(name, age):
-        print(f"hello name:{name},age:{age}")
+    def greet(firstname,lastname, age):
+        print(f"hello name:{firstname}{lastname},age:{age}")
 
     name = get_name()
     age = get_age()
-    greet(name, age)
+    greet(firstname=name['firstname'],lastname=name['lastname'], age=age)
 
 
 first_taskflow = hello()
