db321389529def99b1b9079fe6e9a3bb6198c3f8
AgungTrivaldo <yantosputra121@gmail.com>
2025-12-09 10:50:40 +0700

    check api

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -1,14 +1,25 @@
-from airflow import DAG
-from airflow.decorators import task
-from datetime import datetime
+from airflow.decorators import dag, task
+from airflow.hooks.base import Basehook
+from datetime import datetime, timedelta
+import request
+from airflow.sensors.base import PokeReturnValue
 
-with DAG(
-    dag_id = 'tutorial',
-    schedule_interval = '@once',
-    start_date = datetime(2023, 1, 1),
-    tags=['example'],
-) as dag:
-    
-    @task
-    def test():
-        pass
+
+@dag(
+    dag_id="first_taskflowv1",
+    schedule_interval="@daily",
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+)
+def stock_market():
+    @task.sensor(poke_interval=30, timeout=300, mode="poke")
+    def is_api_available() -> PokeReturnValue:
+        api = Basehook.get_connection("api_connection")
+        url = f"{api.host}{api.extra_dejson['endpoint']}"
+        print(url)
+        response = request.get(url, headers=api .extra_djson['headers'])
+        condition = response.json()['finance']['result'] is None
+        return PokeReturnValue(is_done = condition, xcom_value=url)
+    is_api_available
+
+stock_market()
