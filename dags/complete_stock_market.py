b9ddb365f1dfd679e23d62f26fb5a3dfef014cd0
AgungTrivaldo <yantosputra121@gmail.com>
2025-12-17 11:39:13 +0700

    fetch stock prices

--- dags/complete_stock_market.py
+++ dags/complete_stock_market.py
@@ -3,7 +3,6 @@ from airflow.hooks.base import BaseHook
 from datetime import datetime
 from minio import Minio
 from io import BytesIO
-from concurrent.futures import ThreadPoolExecutor, as_completed
 import requests
 import pandas as pd
 
@@ -46,30 +45,18 @@ def stock_market():
             url = f"{full_url}{symbol}?metrics=high?&interval=1d&range=1y"
             urls.append(url)
         return urls
-    
-    @task
-    def fetch_stock_prices(urls):
+
+    def stock_prices(urls):
         api = BaseHook.get_connection("stock_api")
-        headers = api.extra_dejson.get("headers", {})
         stock_prices = []
-        def fetch(url):
-            try:
-                response = requests.get(url, headers=headers, timeout=10)
-                response.raise_for_status()
-                return response.json()["chart"]["result"][0]
-            except Exception as e:
-                return {"url": url, "error": str(e)}
-
-        # Thread pool for parallel requests
-        with ThreadPoolExecutor(max_workers=64) as executor:
-            futures = {executor.submit(fetch, url): url for url in urls}
-            for future in as_completed(futures):
-                stock_prices.append(future.result())
-
+        for url in urls:
+            response = requests.get(url, headers=api.extra_dejson["headers"])
+            data = response.json()["chart"]["result"][0]
+            stock_prices.append(data)
         return stock_prices
     
     symbols = get_symbol()
     urls = get_link(symbols)
-    fetch_stock_prices(urls)
+    stock_prices = stock_prices(urls)
 
 stock_market()
